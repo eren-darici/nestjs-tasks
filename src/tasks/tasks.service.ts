af5d8f45aaefc4b205b2a44ca3b36576e44a0a08
Eren Darıcı <ed.erendarici@gmail.com>
2022-03-13 03:23:20 +0300

    Add error handling

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -35,7 +35,13 @@ export class TasksService {
   }
 
   public getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
+
+    if (!found) {
+      throw new NotFoundException(`Task with ID ${id} not found!`);
+    }
+
+    return found;
   }
 
   public createTask(createTaskDto: CreateTaskDto): Task {
@@ -54,7 +60,11 @@ export class TasksService {
   }
 
   public deleteTaskById(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    const found = this.getTaskById(id);
+
+    if (found) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
+    }
   }
 
   public updateTaskStatusById(id: string, status: TaskStatus): Task {
